59068deef344b81c08c03e1b91d88ee8a25c3fac
Natalya Krukova <k-natali2009@mail.ru>
2025-11-11 17:44:58 +0300

    add logging to the modules: masks.py and utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,12 @@
+import logging
 from typing import Union
+
+logger = logging.getLogger("masks")
+file_handler = logging.FileHandler("logs/masks.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def get_mask_card_number(card_number: Union[str]) -> Union[str]:
@@ -7,16 +15,21 @@ def get_mask_card_number(card_number: Union[str]) -> Union[str]:
     а вместо остальных ставит **(звездочки)
     """
     card_num = []
+    logger.debug("The card number was received and converted to a string")
     card_number_str = str(card_number)
     star_sp = "** **** "
+    logger.info("The card number digits are hidden from 7 to 12")
     if len(card_number_str) == 16:
+        logger.info("The card has 16 digits")
         for num in card_number_str:
             card_num.append(num)
             card_widget = "".join(card_num[:4]) + " " + "".join(card_num[4:6]) + star_sp + "".join(card_num[-4:])
         return card_widget
     elif len(card_number_str) > 0:
+        logger.warning("Incorrect number of characters, should be 16.")
         return "Incorrect number of characters, should be 16."
     else:
+        logger.error("You haven't entered anything.")
         return "You haven't entered anything."
 
 
@@ -26,14 +39,19 @@ def get_mask_account(account_number: Union[str]) -> Union[str]:
     а вместо последних 5 и 6 цифр ставит **(звездочки)
     """
     account_num = []
+    logger.debug("The account number was received and converted to a string")
     account_number_str = str(account_number)
     star_spice = "**"
+    logger.info("The account number digits are hidden from 1 to 16")
     if len(account_number_str) == 20:
+        logger.info("The account has 20 digits")
         for num in account_number_str:
             account_num.append(num)
             account_widget = star_spice + "".join(account_num[-4:])
         return account_widget
     elif len(account_number_str) > 0:
+        logger.warning("Incorrect number of characters, should be 20.")
         return "Incorrect number of characters, in the account should be 20"
     else:
+        logger.error("You haven't entered anything.")
         return "You haven't entered anything."
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,12 @@
 import json
-from typing import Union, Any
+import logging
+
+logger = logging.getLogger("utils")
+file_handler = logging.FileHandler("logs/utils.log", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def read_json(filename: str) -> tuple[str, set[str]]:
@@ -8,12 +15,17 @@ def read_json(filename: str) -> tuple[str, set[str]]:
     о финансовых транзакциях.
     """
     try:
+        logger.debug(f"Безопасное открытие файла {filename}")
         with open(filename, "r", encoding="utf-8") as f:
+            logger.info(f"файл {filename} имеет JSON-формат")
             operations_file = json.load(f)
             if not isinstance(operations_file, list):
+                logger.warning(f"JSON-файл {filename} должен быть списком")
                 raise TypeError("JSON-файл должен содержать список.")
         return operations_file
     except FileNotFoundError or PermissionError:
-        return "Ошибка: Файл не найден по пути", {filename}
-    except json.JSONDecodeError:
-        return "Ошибка: Файл пустой.", {filename}
+        logger.error(f"Ошибка: файл {filename} не найден по пути")
+        return f"Ошибка: файл {filename} не найден по пути"
+    except json.JSONDecodeError as ex:
+        logger.error(f"Ошибка: Файл {filename} {ex}.")
+        return f"Ошибка: Файл {filename} {ex}."
