6867b0631ece50fc7e7fce8089caf32f4e642f41
Natalya Krukova <k-natali2009@mail.ru>
2025-12-16 11:54:49 +0300

    fixing main.py

--- main.py
+++ main.py
@@ -259,32 +259,3 @@ if __name__ == "__main__":
 
     print(25 * "~**~")
 
-    # print(read_xlsx_file('data/transactions_excel.xlsx'))
-    # csv_file = read_csv_file("data/trans_test.csv")
-    # file_ = [
-    #     {
-    #         "id": 650703.0,
-    #         "state": "EXECUTED",
-    #         "date": "2023-09-05T11:30:32Z",
-    #         "amount": 16210.0,
-    #         "currency_name": "Sol",
-    #         "currency_code": "PEN",
-    #         "from": "Счет 58803664561298323391",
-    #         "to": "Счет 39745660563456619397",
-    #         "description": "Перевод организации",
-    #     },
-    #     {
-    #         "id": 3598919.0,
-    #         "state": "EXECUTED",
-    #         "date": "2020-12-06T23:00:58Z",
-    #         "amount": 29740.0,
-    #         "currency_name": "Peso",
-    #         "currency_code": "COP",
-    #         "from": "Discover 3172601889670065",
-    #         "to": "Discover 0720428384694643",
-    #         "description": "Перевод с карты на карту",
-    #     },
-    # ]
-    # format_card_and_account = format_card_account(csv_file)
-    # print(format_card_and_account)
-    # print(count_transactions(file_, "Перевод организации"))
