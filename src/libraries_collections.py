0a191bb3fcdfe9e45c111a5b911f4f63c6722247
Natalya Krukova <k-natali2009@mail.ru>
2025-12-20 12:41:49 +0300

    fixing func count_transactions

--- src/libraries_collections.py
+++ src/libraries_collections.py
@@ -1,6 +1,6 @@
 import re
 from collections import Counter
-from typing import Any, Iterable, List, Union
+from typing import Any, Iterable, List, Union, Dict
 
 from dateutil.parser import parse
 
@@ -30,21 +30,19 @@ def look_to_dictionary(dict_list: List[dict], string: Union[str]) -> Any:
         return f"Ошибка: {e}"
 
 
-def count_transactions(dict_list: Iterable[dict], class_list: Union[str]) -> Counter:
+def count_transactions(dict_list: List[dict], categories: List[str]) -> Dict[str, int]:
     """
     Функция, которая будет принимать список словарей с данными о банковских операциях и список категорий операций,
     а возвращать словарь, в котором ключи — это названия категорий, а значения — это количество операций в каждой кате-
     гории.
     """
-    filtered_transactions = []  # Создаем пустой список
+    counter = Counter()
     for key in dict_list:  # Итерируемся по списку словарей
-        if (
-            key.get("description") == class_list
-        ):  # Сравниваем заданное значение "class_list" с заданным классом "description"
-            filtered_transactions.append(
-                key["description"]
-            )  # Если сравнение верно, добавляем данный ключ в новый список
-    return dict(Counter(filtered_transactions))  # для подсчета количества банковских операций воспользуемся Counter
+        description_class = key.get("description","") # Проверяем, входит ли описание в список категорий
+        if description_class in categories:
+            counter[description_class] += 1
+    return dict(counter) # для подсчета количества банковских операций воспользуемся Counter
+
 
 
 def look_date(dict_list: Iterable[dict]) -> Any:
@@ -52,4 +50,57 @@ def look_date(dict_list: Iterable[dict]) -> Any:
         date_obj = parse(item["date"])
         formatted_date = date_obj.strftime("%d.%m.%Y")
         return formatted_date
+
+#
+# if __name__ == '__main__':
+#     dict_word = [
+#         {
+#             "id": 939719570,
+#             "state": "EXECUTED",
+#             "date": "2018-06-30T02:08:58.425572",
+#             "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+#             "description": "Перевод организации",
+#             "from": "Счет 75106830613657916952",
+#             "to": "Счет 11776614605963066702",
+#         },
+#         {
+#             "id": 142264268,
+#             "state": "EXECUTED",
+#             "date": "2019-04-04T23:20:05.206878",
+#             "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+#             "description": "Перевод со счета на счет",
+#             "from": "Счет 19708645243227258542",
+#             "to": "Счет 75651667383060284188",
+#         },
+#         {
+#             "id": 873106923,
+#             "state": "EXECUTED",
+#             "date": "2019-03-23T01:09:46.296404",
+#             "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+#             "description": "Перевод со счета на счет",
+#             "from": "Счет 44812258784861134719",
+#             "to": "Счет 74489636417521191160",
+#         },
+#         {
+#             "id": 895315941,
+#             "state": "EXECUTED",
+#             "date": "2018-08-19T04:27:37.904916",
+#             "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+#             "description": "Перевод с карты на карту",
+#             "from": "Visa Classic 6831982476737658",
+#             "to": "Visa Platinum 8990922113665229",
+#         },
+#         {
+#             "id": 594226727,
+#             "state": "CANCELED",
+#             "date": "2018-09-12T21:27:25.241689",
+#             "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+#             "description": "Перевод организации",
+#             "from": "Visa Platinum 1246377376343588",
+#             "to": "Счет 14211924144426031657",
+#         },
+#     ]
+#     categories = ["Перевод с карты на карту","Перевод организации"]
+#     result = count_transactions(dict_word,categories)
+#     print(result)
 
